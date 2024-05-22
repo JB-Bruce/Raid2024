eaaf894bd9df4eac0d3114f5c4a392124e6de3f4
Don Keiser <sworktale@gmail.com>
2024-05-22 12:27:19 +0200

    Main Menu Part 1 & 2
    -Added Main Menu, that includes buttons to play the game, exit the game, go to settings and credits
    -Added Settings sub-menu.
    Settings allow the change of volume for the main Volume, Music volume and SFX volume.
    -Added a sound manager that allows for Musics and SFX to be called

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public static SettingsMenu instance;
+
+    [Header("Sliders: ")]
+    [SerializeField] Slider mainVolumeSlider;
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider sfxVolumeSlider;
+
+    [Header("References: ")]
+    public GameObject BackButton;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            transform.parent.GetComponent<Canvas>().sortingOrder = 50;
+            transform.parent.GetComponent<Canvas>().worldCamera = Camera.main;
+
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Destroy(transform.parent.parent.gameObject);
+        }
+
+        DontDestroyOnLoad(transform.parent.parent); ;
+
+        //Links volume sliders to their respective functions
+        mainVolumeSlider.onValueChanged.AddListener(delegate { OnMainVolumeChange(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+        sfxVolumeSlider.onValueChanged.AddListener(delegate { OnSFXVolumeChange(); });
+
+    }
+
+    public void OnMainVolumeChange()
+    {
+        SoundManager.instance.MainVolume(mainVolumeSlider.value);
+    }
+
+    public void OnMusicVolumeChange()
+    {
+        SoundManager.instance.MusicVolume(musicVolumeSlider.value);
+    }
+
+    public void OnSFXVolumeChange()
+    {
+        SoundManager.instance.SFXVolume(sfxVolumeSlider.value);
+    }
+}
---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [Header("References: ")]
+    [SerializeField] AudioSource musicPlayer;
+    [SerializeField] AudioSource sfxPlayer;
+
+    [Header("Clips: ")]
+    [SerializeField] Sounds[] musics, sfxs;
+
+    SettingsMenu settingsMenu;
+
+    private float musicVolume = 1f;
+    private float sfxVolume = 1f;
+    private float mainVolume = 1f; // Global volume multiplier
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        settingsMenu = SettingsMenu.instance;
+        PlayMusic("Theme");
+    }
+
+    public void PlayMusic(string _name)
+    {
+        Sounds _s = Array.Find(musics, _x =>  _x.name == _name);
+        if (_s == null) //If doesn't find music by name
+        {
+            Debug.Log("Music not Found");
+        }
+        else
+        {
+            musicPlayer.clip = _s.clip;
+            musicPlayer.Play();
+        }
+    }
+
+    public void PlaySFX(string _name)
+    {
+        Sounds _s = Array.Find(sfxs, _x => _x.name == _name);
+        if (_s == null) //If doesn't find sfx by name
+        {
+            Debug.Log("SFX not Found");
+        }
+        else
+        {
+            sfxPlayer.PlayOneShot(_s.clip);
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        musicPlayer.mute = !musicPlayer.mute;
+    }
+
+    public void ToggleSFX()
+    {
+        sfxPlayer.mute = !sfxPlayer.mute;
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicVolume = volume;
+        UpdateVolumes();
+    }
+
+    public void SFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        UpdateVolumes();
+    }
+
+    public void MainVolume (float volume)
+    {
+        mainVolume = volume;
+        UpdateVolumes();
+    }
+
+    private void UpdateVolumes()
+    {
+        musicPlayer.volume = musicVolume * mainVolume;
+        sfxPlayer.volume = sfxVolume * mainVolume;
+    }
+}
