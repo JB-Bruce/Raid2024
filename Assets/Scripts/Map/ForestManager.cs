3c7f4fadcf56d622c2081380d32db725946ee25b
MDucazeaux <m.ducazeaux@lahorde.school>
2024-06-03 13:58:47 +0200

    Random Tree Sprite
    -Add function to random the sprite and the size of each tree

---
+++ Assets/Scripts/Map/ForestManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ForestManager : MonoBehaviour
+{
+
+    public List<Transform> ForestTrees = new List<Transform>(); 
+
+    public List<Tree> Prefabs = new List<Tree>();
+
+    private float _minSizetree = 0.9f;
+    private float _maxSizetree = 1.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomTree();
+    }
+
+
+
+    //Random the size and the sprite of the the tree in the list
+    private void RandomTree()
+    {
+
+        foreach (Transform t in ForestTrees)
+        {
+            float size = Random.Range(_minSizetree, _maxSizetree + 0.01f);
+            float randompercentage = Random.Range(0, 100.0f);
+            float prefabspercentage = 0;
+            float lastpercentage = 0;
+
+
+            foreach (Tree tree in Prefabs)
+            {
+                lastpercentage = prefabspercentage;
+                prefabspercentage += tree.Percentage;
+                if (prefabspercentage >= randompercentage && randompercentage >= lastpercentage)
+                {
+                    t.GetComponent<SpriteRenderer>().sprite = tree.SpriteTree;
+                }
+            }
+
+            t.transform.localScale = new Vector3(size, size, size);
+        }
+    }
+
+
+    //contain the sprite and the percentage of a tree
+    [System.Serializable]
+    public struct Tree
+    {
+        public Sprite SpriteTree;
+        public float Percentage;
+    }
+}
