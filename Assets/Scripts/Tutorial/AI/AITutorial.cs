f5d9afea04270fcc8e48ca989ed25260bddd7b25
MDucazeaux <m.ducazeaux@lahorde.school>
2024-06-05 11:58:20 +0200

    Step 4 tutorial
    -The player need to take down the enemy to get to the next step

---
+++ Assets/Scripts/Tutorial/AI/AITutorial.cs
@@ -0,0 +1,42 @@
+using NavMeshPlus.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AITutorial : MonoBehaviour
+{
+    public Transform Player;
+
+    [SerializeField] private float _lerptime = 10;
+
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       _transform.position =  Vector3.Lerp(transform.position, Player.position, (1 * Time.deltaTime) / _lerptime);
+            
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Bullet>())
+        {
+            TutorialManager.Instance.NextTutorial();
+            _transform.gameObject.SetActive(false);
+        }
+    }
+}
