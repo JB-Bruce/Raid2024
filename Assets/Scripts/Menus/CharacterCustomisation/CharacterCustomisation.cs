bc91beb2ddc28c42f5969f8fb34385c7eba8ca59
AlexisVildy <alexisvildy@gmail.com>
2024-06-21 11:40:55 +0200

    FixNavigationTuto

--- Assets/Scripts/Menus/CharacterCustomisation/CharacterCustomisation.cs
+++ Assets/Scripts/Menus/CharacterCustomisation/CharacterCustomisation.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering.VirtualTexturing;
 using UnityEngine.UI;
 
 public class CharacterCustomisation : MonoBehaviour
